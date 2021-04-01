b91c3a30d27a1fa45d62893805bf2446a800103c
sepehr ansaripour <pehrans@gmail.com>
2021-04-01 15:09:28 -0400

    Fix python tests

--- property-based-testing/python-pbt/test/ebtest_upper.py
+++ property-based-testing/python-pbt/test/ebtest_upper.py
@@ -3,13 +3,13 @@ import unittest
 
 class TestUpper(unittest.TestCase):
     def test_upper_maintains_length(self):
-        self.assertEqual(len('CACA'), len('caca'.upper()))
+        self.assertEqual(len('caca'), len('caca'.upper()))
 
     def test_upper_less_than_lower(self):
         self.assertLess('c'.upper(), 'c')
 
     def test_upper_avoids_non_lower(self):
-        self.assertEqual('ABC123', 'abc123'.upper())
+        self.assertEqual('ABC123', 'AbC123'.upper())
 
 
 if __name__ == '__main__':
--- property-based-testing/python-pbt/test/pbtest_upper.py
+++ property-based-testing/python-pbt/test/pbtest_upper.py
@@ -1,7 +1,7 @@
 import random
 import re
 import unittest
-from hypothesis import given, settings, reproduce_failure
+from hypothesis import assume, given, settings, reproduce_failure
 from hypothesis.strategies import text, tuples, characters
 import string
 
@@ -14,17 +14,21 @@ class TestUpper(unittest.TestCase):
     def test_upper_maintains_length(self, s):
         self.assertEqual(len(s.upper()), len(s))
 
+    # Ll stands for letter, lowercase and helps to generates lowercase letters
     @given(text(alphabet=characters(whitelist_categories=['Ll'])))
     @settings(max_examples=max_examples, print_blob=True)
     def test_upper_less_than_lower(self, s):
-        list(map(lambda c: self.assertLess(c.upper(), c), s))
+        assume(s)
+        self.assertLess(s.upper(), s)
 
     @given(tuples(text(alphabet=characters(whitelist_categories=['Ll'])),
+                  # Uppercase characters
                   text(alphabet=characters(blacklist_categories=['Ll']))).map(
         lambda t: (t[0], t[1], ''.join(random.sample(t[0] + t[1], len(t[0] + t[1]))))))
     @settings(max_examples=max_examples, print_blob=True)
     def test_upper_avoids_non_lower(self, t):
         lower, other, shuffled = t
+	# Capitalizes lowercase characters, character by character
         expected = ''.join(list(map(lambda c: c.upper() if c in lower else c, shuffled)))
         self.assertEqual(expected, shuffled.upper())
 
